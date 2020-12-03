f5bef720567df8f411e65c30a9013b72cb49a33e
Simon Willison <swillison@gmail.com>
2020-12-03 14:57:01 -0800

    Swap order of comparison

--- {{cookiecutter.hyphenated}}/tests/test_{{cookiecutter.underscored}}.py
+++ {{cookiecutter.hyphenated}}/tests/test_{{cookiecutter.underscored}}.py
@@ -6,5 +6,5 @@ def test_version():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(cli, ["--version"])
-        assert 0 == result.exit_code
+        assert result.exit_code == 0
         assert result.output.startswith("cli, version ")
